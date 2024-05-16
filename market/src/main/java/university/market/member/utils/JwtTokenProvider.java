10c9e0eecb40bee6f8ab9d24ef1ef2efe867a8d0
kbs1027 <rlaqjatn1027@gmail.com>
2024-05-17 02:35:55 +0900

    feat : token 정보 삭제

--- market/src/main/java/university/market/member/utils/JwtTokenProvider.java
+++ market/src/main/java/university/market/member/utils/JwtTokenProvider.java
@@ -11,8 +11,6 @@ import university.market.member.exception.LoginExceptionType;
 
 @Component
 public class JwtTokenProvider {
-    @Value("${spring.application.name}")
-    private String issuer;
     @Value("${security.jwt.secret-key}")
     private String secretKey;
     @Value("${security.jwt.expire-length}")
@@ -20,12 +18,9 @@ public class JwtTokenProvider {
 
     public String generateToken(final String email) {
         final Date now = new Date();
-        final Date expiredDate = new Date(now.getTime() + expireTimeMilliSecond);
 
         return Jwts.builder()
                 .claim("email", email)
-                .issuer(issuer)
-                .issuedAt(now)
                 .expiration(new Date(now.getTime() + expireTimeMilliSecond))
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
