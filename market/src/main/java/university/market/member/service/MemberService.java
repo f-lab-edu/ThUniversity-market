6792e2637e27742d19ae86076797084e9a9e76b9
kbs1027 <rlaqjatn1027@gmail.com>
2024-05-17 02:38:01 +0900

    feat : 비밀번호 암호화 적용

--- market/src/main/java/university/market/member/service/MemberService.java
+++ market/src/main/java/university/market/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package university.market.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import university.market.member.domain.MemberVO;
@@ -9,6 +10,7 @@ import university.market.member.exception.LoginExceptionType;
 import university.market.member.service.dto.request.JoinRequest;
 import university.market.member.mapper.MemberMapper;
 import university.market.member.service.dto.request.LoginRequest;
+import university.market.member.service.dto.response.LoginResponse;
 import university.market.member.utils.JwtTokenProvider;
 
 @Service
@@ -17,29 +19,34 @@ public class MemberService {
 
     private final MemberMapper memberMapper;
     private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public boolean joinMember(JoinRequest joinRequest) {
-        if(memberMapper.findMemberByEmail(joinRequest.getEmail()) != null) {
+        if(memberMapper.findMemberByEmail(joinRequest.email()) != null) {
             throw new LoginException(LoginExceptionType.ALREADY_EXISTED_MEMBER);
         }
 
         MemberVO memberVO = MemberVO.builder()
-                .name(joinRequest.getName())
-                .email(joinRequest.getEmail())
-                .password(joinRequest.getPassword())
-                .university(joinRequest.getUniversity())
+                .name(joinRequest.name())
+                .email(joinRequest.email())
+                .password(passwordEncoder.encode(joinRequest.password()))
+                .university(joinRequest.university())
                 .build();
 
         return memberMapper.joinMember(memberVO);
     }
 
     @Transactional(readOnly = true)
-    public String loginMember(LoginRequest loginRequest) {
-        MemberVO member = memberMapper.loginMember(loginRequest.getEmail(), loginRequest.getPassword());
-        if (member == null) {
+    public LoginResponse loginMember(LoginRequest loginRequest) {
+        MemberVO member = memberMapper.findMemberByEmail(loginRequest.email());
+        if (member == null || !passwordEncoder.matches(loginRequest.password(), member.getPassword())) {
             throw new LoginException(LoginExceptionType.INVALID_LOGIN_CREDENTIALS);
         }
-        return jwtTokenProvider.generateToken(member.getEmail());
+
+        return LoginResponse.builder()
+                .memberId(member.getId())
+                .token(jwtTokenProvider.generateToken(member.getEmail()))
+                .build();
     }
 }
