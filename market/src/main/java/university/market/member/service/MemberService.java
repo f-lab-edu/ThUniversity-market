01d09ea0e1c5543fb73d0ba7363157f4397e8871
kbs1027 <rlaqjatn1027@gmail.com>
2024-05-22 13:32:47 +0900

    refactor : 함수 return 값 변경

--- market/src/main/java/university/market/member/service/MemberService.java
+++ market/src/main/java/university/market/member/service/MemberService.java
@@ -6,6 +6,6 @@ import university.market.member.service.dto.request.LoginRequest;
 import university.market.member.service.dto.response.LoginResponse;
 
 public interface MemberService {
-    public boolean joinMember(JoinRequest joinRequest);
+    public void joinMember(JoinRequest joinRequest);
     public LoginResponse loginMember(LoginRequest loginRequest);
 }
