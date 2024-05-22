4ecfad91813950130829b8d583eb24f9e4f4ed3c
kbs1027 <rlaqjatn1027@gmail.com>
2024-05-22 13:32:10 +0900

    feat : 예외처리 추가 및 변수 final

--- market/src/main/java/university/market/member/service/MemberServiceImpl.java
+++ market/src/main/java/university/market/member/service/MemberServiceImpl.java
@@ -7,39 +7,43 @@ import org.springframework.transaction.annotation.Transactional;
 import university.market.member.domain.MemberVO;
 import university.market.member.exception.MemberException;
 import university.market.member.exception.MemberExceptionType;
-import university.market.member.service.dto.request.JoinRequest;
 import university.market.member.mapper.MemberMapper;
+import university.market.member.service.dto.request.JoinRequest;
 import university.market.member.service.dto.request.LoginRequest;
 import university.market.member.service.dto.response.LoginResponse;
 import university.market.member.utils.JwtTokenProvider;
 
 @Service
 @RequiredArgsConstructor
-public class MemberServiceImpl implements MemberService{
+public class MemberServiceImpl implements MemberService {
 
     private final MemberMapper memberMapper;
     private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public boolean joinMember(JoinRequest joinRequest) {
-        if(memberMapper.findMemberByEmail(joinRequest.email()) != null) {
+    public void joinMember(JoinRequest joinRequest) {
+        if (memberMapper.findMemberByEmail(joinRequest.email()) != null) {
             throw new MemberException(MemberExceptionType.ALREADY_EXISTED_MEMBER);
         }
 
-        MemberVO memberVO = MemberVO.builder()
+        final MemberVO memberVO = MemberVO.builder()
                 .name(joinRequest.name())
                 .email(joinRequest.email())
                 .password(passwordEncoder.encode(joinRequest.password()))
                 .university(joinRequest.university())
                 .build();
 
-        return memberMapper.joinMember(memberVO);
+        try {
+            memberMapper.joinMember(memberVO);
+        } catch (Exception e) {
+            throw new MemberException(MemberExceptionType.DATABASE_ERROR);
+        }
     }
 
     @Transactional(readOnly = true)
     public LoginResponse loginMember(LoginRequest loginRequest) {
-        MemberVO member = memberMapper.findMemberByEmail(loginRequest.email());
+        final MemberVO member = memberMapper.findMemberByEmail(loginRequest.email());
         if (member == null || !passwordEncoder.matches(loginRequest.password(), member.getPassword())) {
             throw new MemberException(MemberExceptionType.INVALID_LOGIN_CREDENTIALS);
         }
