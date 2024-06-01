0aa78cbbdf70515c99d6b395acda801c668716f0
kbs1027 <rlaqjatn1027@gmail.com>
2024-06-01 18:53:08 +0900

    feat : sql 자동 생성 추가

---
+++ market/src/main/resources/sql/schema.sql
@@ -0,0 +1,12 @@
+drop table if exists market.member;
+
+create table member(
+      id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      name varchar(255) NOT NULL,
+      email varchar(255) NOT NULL,
+      password varchar(255) NOT NULL,
+      university int NOT NULL,
+      auth enum('ROLE_USER','ROLE_ADMIN') NOT NULL,
+      created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+      updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
