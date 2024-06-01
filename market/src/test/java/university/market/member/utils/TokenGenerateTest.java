15e9da85d6a91fab6f693ec534d24c92f8b4f065
kbs1027 <rlaqjatn1027@gmail.com>
2024-06-01 19:46:42 +0900

    refactor : 통합 테스트를 단위 테스트로 변경

--- market/src/test/java/university/market/member/utils/TokenGenerateTest.java
+++ market/src/test/java/university/market/member/utils/TokenGenerateTest.java
@@ -1,57 +1,68 @@
 package university.market.member.utils;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import lombok.extern.slf4j.Slf4j;
 import university.market.member.exception.MemberException;
-
-import static org.assertj.core.api.Assertions.*;
+import university.market.member.exception.MemberExceptionType;
 
 @Slf4j
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class TokenGenerateTest {
 
-    @Autowired
+    @Mock
     private JwtTokenProvider jwtTokenProvider;
 
     @Test
     @DisplayName("[success] login success generate token")
     void generateToken_login_success_generate_token() {
-        //given
+        // given
         final String memberEmail = "test@test";
+        final String token = "testToken";
 
-        //when
-        final String token = jwtTokenProvider.generateToken(memberEmail);
-        final String extractedMemberEmail = jwtTokenProvider.extractEmail(token);
+        // mocking
+        when(jwtTokenProvider.generateToken(memberEmail)).thenReturn(token);
+        when(jwtTokenProvider.extractEmail(token)).thenReturn(memberEmail);
 
-        //then
+        // when
+        final String generatedToken = jwtTokenProvider.generateToken(memberEmail);
+        final String extractedMemberEmail = jwtTokenProvider.extractEmail(generatedToken);
+
+        // then
         assertThat(extractedMemberEmail).isEqualTo(memberEmail);
-        log.info("Access Token : {}", token);
+        log.info("Access Token : {}", generatedToken);
     }
 
     @Test
     @DisplayName("[fail] token expired")
-    void token_expired_not_valid() throws InterruptedException {
-        //given
+    void token_expired_not_valid() {
+        // given
         final String memberEmail = "test@test";
-
-        //when
+        final String token = "expiredToken";
         final LocalDateTime now = LocalDateTime.now();
         final long expireTime = 1000L;
 
-        ExpireDateSupplier expireDateSupplier = () -> Date.from(LocalDateTime.from(now.plusSeconds(expireTime/1000L)).atZone(
-                ZoneId.systemDefault()).toInstant());
-        final String token = jwtTokenProvider.generateToken(memberEmail, expireDateSupplier);
+        ExpireDateSupplier expireDateSupplier = () -> Date.from(LocalDateTime.from(now.minusSeconds(expireTime))
+                .atZone(ZoneId.systemDefault()).toInstant());
 
-        Thread.sleep(expireTime + 1000L);
+        // mocking
+        when(jwtTokenProvider.generateToken(memberEmail, expireDateSupplier)).thenReturn(token);
+        doThrow(new MemberException(MemberExceptionType.EXPIRED_ACCESS_TOKEN)).when(jwtTokenProvider).validateToken(token);
+
+        // when
+        final String generatedToken = jwtTokenProvider.generateToken(memberEmail, expireDateSupplier);
+
         // then
-
-        assertThatThrownBy(() -> jwtTokenProvider.validateToken(token))
+        assertThatCode(() -> jwtTokenProvider.validateToken(generatedToken))
                 .isInstanceOf(MemberException.class)
                 .hasMessage("만료된 Access Token입니다.");
     }
