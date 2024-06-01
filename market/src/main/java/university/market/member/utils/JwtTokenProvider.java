85f7910c5d292a76c8f72fd964ada98328eb3a02
kbs1027 <rlaqjatn1027@gmail.com>
2024-06-01 19:45:24 +0900

    refactor : interface 변수 형태 일관화

--- market/src/main/java/university/market/member/utils/JwtTokenProvider.java
+++ market/src/main/java/university/market/member/utils/JwtTokenProvider.java
@@ -1,12 +1,12 @@
 package university.market.member.utils;
 public interface JwtTokenProvider {
 
-    String generateToken(final String email);
+    String generateToken(String email);
 
-    String generateToken(final String email, final ExpireDateSupplier expireDateSupplier);
+    String generateToken(String email, ExpireDateSupplier expireDateSupplier);
 
     void validateToken(String token);
 
-    String extractEmail(final String token);
+    String extractEmail(String token);
 
 }
