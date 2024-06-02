156c8100d172488f09892bf72d539c7b3d55ed4b
kbs1027 <rlaqjatn1027@gmail.com>
2024-06-03 04:03:23 +0900

    feat : email exception 구현

---
+++ market/src/main/java/university/market/email/exception/EmailException.java
@@ -0,0 +1,20 @@
+package university.market.email.exception;
+
+import university.market.base.exception.BaseException;
+import university.market.base.exception.BaseExceptionType;
+
+public class EmailException extends BaseException {
+
+    private final EmailExceptionType exceptionType;
+
+    public EmailException(final EmailExceptionType exceptionType) {
+        super(exceptionType.errorMessage());
+        this.exceptionType = exceptionType;
+    }
+
+
+    @Override
+    public BaseExceptionType exceptionType() {
+        return exceptionType;
+    }
+}
