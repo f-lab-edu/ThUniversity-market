7ee3622b5ba1bdc5b0d7e6921a9c442fa412cef3
kbs1027 <rlaqjatn1027@gmail.com>
2024-06-03 04:02:11 +0900

    feat : 데이터 베이스 email 스키마 추가

--- market/src/main/resources/sql/schema.sql
+++ market/src/main/resources/sql/schema.sql
@@ -1,4 +1,5 @@
 drop table if exists market.member;
+drop table if exists market.email;
 
 create table member(
       id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -10,3 +11,19 @@ create table member(
       created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
       updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+create table email(
+    id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email varchar(255) NOT NULL,
+    verification_code varchar(255) NOT NULL,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+SET GLOBAL event_scheduler = ON;
+
+CREATE EVENT delete_old_emails
+ON SCHEDULE EVERY 1 MINUTE
+DO
+DELETE FROM email
+WHERE created_at < NOW() - INTERVAL 1 MINUTE;
