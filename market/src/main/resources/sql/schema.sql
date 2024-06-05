e4b76d3e1d35e3cdc1c3b696c35a8f83af19ad1c
kbs1027 <rlaqjatn1027@gmail.com>
2024-06-05 20:46:43 +0900

    feat : member 테이블에 email_verify column 추가

--- market/src/main/resources/sql/schema.sql
+++ market/src/main/resources/sql/schema.sql
@@ -7,7 +7,8 @@ create table member(
       email varchar(255) NOT NULL,
       password varchar(255) NOT NULL,
       university int NOT NULL,
-      auth enum('ROLE_USER','ROLE_ADMIN') NOT NULL,
+      auth enum('ROLE_USER','ROLE_VERIFY_USER','ROLE_ADMIN') NOT NULL,
+      email_verify boolean NOT NULL,
       created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
       updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
