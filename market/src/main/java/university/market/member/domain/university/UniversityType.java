45f0facc1be5bf40cd0c39a4215cf8d61c640d3e
kbs1027 <rlaqjatn1027@gmail.com>
2024-06-06 16:17:20 +0900

    refactor : getter annotation으로 변환

--- market/src/main/java/university/market/member/domain/university/UniversityType.java
+++ market/src/main/java/university/market/member/domain/university/UniversityType.java
@@ -1,19 +1,19 @@
 package university.market.member.domain.university;
 
+import lombok.Getter;
+
+@Getter
 public enum UniversityType {
     ADMIN(0),
     PUSAN(1),
     SEOUL(2),
     YONSEI(3),
     KOREA(4);
+    
     private final int value;
 
     UniversityType(int value) {
         this.value = value;
-    }
-
-    public int getValue() {
-        return value;
     }
 
     public static UniversityType fromValue(int value) {
