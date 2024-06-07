a64a92b344e1e5baa82360472898f5ce769fc9a1
kbs1027 <rlaqjatn1027@gmail.com>
2024-06-07 17:42:11 +0900

    feat : item 데이터베이스 schema 추가

--- market/src/main/resources/sql/schema.sql
+++ market/src/main/resources/sql/schema.sql
@@ -1,3 +1,4 @@
+drop table if exists market.item;
 drop table if exists market.member;
 
 create table member(
@@ -10,3 +11,19 @@ create table member(
       created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
       updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+create table item(
+    id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title varchar(255) NOT NULL,
+    description TEXT NOT NULL,
+    image_url varchar(255) NOT NULL,
+    seller bigint NOT NULL,
+    status enum('SELLING', 'TRADING', 'FINISH') NOT NULL,
+    auction boolean NOT NULL,
+    price int NOT NULL,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (seller) REFERENCES member(id)
+);
+
+
