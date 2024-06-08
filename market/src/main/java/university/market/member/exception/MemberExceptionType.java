394c74a19c7cf5eca9082eacf9605f73da36c4d6
kbs1027 <rlaqjatn1027@gmail.com>
2024-06-08 18:57:38 +0900

    feat : MemberException 추가

--- market/src/main/java/university/market/member/exception/MemberExceptionType.java
+++ market/src/main/java/university/market/member/exception/MemberExceptionType.java
@@ -8,7 +8,10 @@ public enum MemberExceptionType implements BaseExceptionType {
     INVALID_LOGIN_CREDENTIALS(HttpStatus.UNAUTHORIZED, "유효하지 않은 로그인 정보입니다.", 401101),
     EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 Access Token입니다.", 401102),
     INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 Access Token입니다.", 401103),
-    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 오류가 발생했습니다.", 500104);
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다.", 404104),
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 오류가 발생했습니다.", 500105),
+    SESSION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "세션 오류가 발생했습니다.", 500106),
+    INVALID_CASTING(HttpStatus.INTERNAL_SERVER_ERROR, "유효하지 않은 형변환입니다.", 500107);
 
     private final HttpStatus httpStatus;
     private final String errorMessage;
