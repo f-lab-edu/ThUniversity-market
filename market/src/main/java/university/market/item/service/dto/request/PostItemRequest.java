ae1dba01980274b795e19e11f014fcb4e9c31bbb
kbs1027 <rlaqjatn1027@gmail.com>
2024-06-13 00:42:02 +0900

    refactor : dto에서 memberToken 제거

--- market/src/main/java/university/market/item/service/dto/request/PostItemRequest.java
+++ market/src/main/java/university/market/item/service/dto/request/PostItemRequest.java
@@ -3,7 +3,6 @@ package university.market.item.service.dto.request;
 public record PostItemRequest(
         String title,
         String description,
-        String memberToken,
         boolean auction,
         int price
 
--- market/src/main/java/university/market/item/service/dto/request/UpdateItemRequest.java
+++ market/src/main/java/university/market/item/service/dto/request/UpdateItemRequest.java
@@ -4,7 +4,6 @@ public record UpdateItemRequest(
         Long itemId,
         String title,
         String description,
-        String memberToken,
         String status,
         boolean auction,
         int price
