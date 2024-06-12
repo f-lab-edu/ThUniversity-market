ffd3dd4a1ff9e42a358d5038e1a4c2bc2417b33c
kbs1027 <rlaqjatn1027@gmail.com>
2024-06-13 03:40:01 +0900

    feat : dibs service 및 controller 구현

---
+++ market/src/main/java/university/market/dibs/controller/DibsController.java
@@ -0,0 +1,43 @@
+package university.market.dibs.controller;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import university.market.dibs.domain.DibsVO;
+import university.market.dibs.service.DibsService;
+import university.market.member.annotation.AuthCheck;
+import university.market.member.domain.auth.AuthType;
+
+@RestController
+@RequestMapping("/dibs")
+@RequiredArgsConstructor
+public class DibsController {
+
+    private final DibsService dibsService;
+
+    @AuthCheck({AuthType.ROLE_VERIFY_USER, AuthType.ROLE_ADMIN})
+    @GetMapping("/{itemId}")
+    public ResponseEntity<Void> addDibs(@PathVariable Long itemId) {
+        dibsService.addDibs(itemId);
+        return ResponseEntity.ok().build();
+    }
+
+    @AuthCheck({AuthType.ROLE_VERIFY_USER, AuthType.ROLE_ADMIN})
+    @DeleteMapping("/{dibsId}")
+    public ResponseEntity<Void> removeDibs(@PathVariable Long dibsId) {
+        dibsService.removeDibs(dibsId);
+        return ResponseEntity.ok().build();
+    }
+
+    @AuthCheck({AuthType.ROLE_VERIFY_USER, AuthType.ROLE_ADMIN})
+    @GetMapping("/list")
+    public ResponseEntity<List<DibsVO>> getDibsByMemberId() {
+        List<DibsVO> dibsList = dibsService.getDibsByMemberId();
+        return ResponseEntity.ok(dibsList);
+    }
+}
---
+++ market/src/main/java/university/market/dibs/service/DibsServiceImpl.java
@@ -0,0 +1,59 @@
+package university.market.dibs.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import university.market.dibs.domain.DibsVO;
+import university.market.dibs.exception.DibsException;
+import university.market.dibs.exception.DibsExceptionType;
+import university.market.dibs.mapper.DibsMapper;
+import university.market.item.domain.ItemVO;
+import university.market.item.service.ItemService;
+import university.market.member.annotation.AuthCheck;
+import university.market.member.domain.MemberVO;
+import university.market.member.domain.auth.AuthType;
+import university.market.member.utils.http.HttpRequest;
+
+@Service
+@RequiredArgsConstructor
+public class DibsServiceImpl implements DibsService {
+    private HttpRequest httpRequest;
+
+    private ItemService itemService;
+
+    private DibsMapper dibsMapper;
+
+    @AuthCheck({AuthType.ROLE_VERIFY_USER, AuthType.ROLE_ADMIN})
+    @Transactional
+    @Override
+    public void addDibs(Long itemId) {
+        MemberVO member = httpRequest.getCurrentMember();
+        ItemVO item = itemService.getItemById(itemId);
+
+        if (member.equals(item.getSeller())) {
+            throw new DibsException(DibsExceptionType.NO_DIBS_MYSELF);
+        }
+
+        DibsVO dibsVO = DibsVO.builder()
+            .member(member)
+            .item(item)
+            .build();
+
+        dibsMapper.addDibs(dibsVO);
+    }
+
+    @AuthCheck({AuthType.ROLE_VERIFY_USER, AuthType.ROLE_ADMIN})
+    @Transactional
+    @Override
+    public void removeDibs(Long dibsId) {
+        dibsMapper.removeDibs(dibsId);
+    }
+
+    @AuthCheck({AuthType.ROLE_VERIFY_USER, AuthType.ROLE_ADMIN})
+    @Transactional(readOnly = true)
+    @Override
+    public List<DibsVO> getDibsByMemberId() {
+        return dibsMapper.getDibsByMemberId(httpRequest.getCurrentMember().getId());
+    }
+}
