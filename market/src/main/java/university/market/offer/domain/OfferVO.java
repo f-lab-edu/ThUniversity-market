186054d194bc022143b73ee0e0cd943565136410
kbs1027 <rlaqjatn1027@gmail.com>
2024-06-16 20:16:46 +0900

    feat : offer 테이블 필드 변경

--- market/src/main/java/university/market/offer/domain/OfferVO.java
+++ market/src/main/java/university/market/offer/domain/OfferVO.java
@@ -1,6 +1,7 @@
 package university.market.offer.domain;
 
 import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import university.market.member.domain.MemberVO;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class OfferVO {
     Long id;
 
@@ -18,17 +20,20 @@ public class OfferVO {
 
     int price;
 
-    boolean accepted;
+    OfferStatus status;
+
+    boolean isDeleted;
 
     Timestamp createdAt;
 
     Timestamp updatedAt;
 
     @Builder
-    public OfferVO(ItemVO item, MemberVO buyer, int price) {
+    public OfferVO(ItemVO item, MemberVO buyer, int price, OfferStatus status) {
         this.item = item;
         this.buyer = buyer;
         this.price = price;
-        this.accepted = false;
+        this.status = status;
+        this.isDeleted = false;
     }
 }
