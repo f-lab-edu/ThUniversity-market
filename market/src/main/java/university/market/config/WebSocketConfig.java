b3ae8951e26e0b4c48158bb967267247d53f3804
kbs1027 <rlaqjatn1027@gmail.com>
2024-07-03 16:44:44 +0900

    feat : socket config 구현

---
+++ market/src/main/java/university/market/config/WebSocketConfig.java
@@ -0,0 +1,37 @@
+package university.market.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import university.market.config.interceptor.MessagePreHandler;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Autowired
+    private MessagePreHandler messagePreHandler;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub");
+        config.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/connection")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+        registry.addEndpoint("/connection")
+                .setAllowedOriginPatterns("*");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(messagePreHandler);
+    }
+}
