ae557504193c99410e1fa17c6f6e02e78f47680c
kbs1027 <rlaqjatn1027@gmail.com>
2024-07-11 19:48:13 +0900

    feat : member state 구현 구문 추가

--- market/src/main/java/university/market/member/mapper/MemberMapper.java
+++ market/src/main/java/university/market/member/mapper/MemberMapper.java
@@ -3,6 +3,7 @@ package university.market.member.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import university.market.member.domain.MemberVO;
 import university.market.member.domain.auth.AuthType;
+import university.market.member.domain.memberstatus.MemberStatus;
 
 @Mapper
 public interface MemberMapper {
@@ -15,4 +16,6 @@ public interface MemberMapper {
     void deleteMemberById(Long id);
 
     void updateAuth(String email, AuthType auth);
+
+    void updateMemberStatus(Long id, MemberStatus memberStatus);
 }
