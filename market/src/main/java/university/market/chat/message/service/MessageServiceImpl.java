b5e793287872008a001eb75f16542b62409cae01
kbs1027 <rlaqjatn1027@gmail.com>
2024-07-22 19:37:20 +0900

    refactor : AuthCheck 어노테이션 적용

--- market/src/main/java/university/market/chat/message/service/MessageServiceImpl.java
+++ market/src/main/java/university/market/chat/message/service/MessageServiceImpl.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import university.market.chat.message.domain.MessageVO;
 import university.market.chat.message.mapper.MessageMapper;
 import university.market.chat.message.service.dto.request.MessageRequest;
@@ -11,7 +12,9 @@ import university.market.chat.room.exception.ChatException;
 import university.market.chat.room.exception.ChatExceptionType;
 import university.market.chat.room.mapper.ChatMemberMapper;
 import university.market.chat.room.service.ChatService;
+import university.market.member.annotation.AuthCheck;
 import university.market.member.domain.MemberVO;
+import university.market.member.domain.auth.AuthType;
 import university.market.member.utils.auth.PermissionCheck;
 
 @Service
@@ -22,9 +25,13 @@ public class MessageServiceImpl implements MessageService {
     private final ChatService chatService;
     private final PermissionCheck permissionCheck;
 
-    //    @AuthCheck({AuthType.ROLE_ADMIN, AuthType.ROLE_VERIFY_USER})
+    @AuthCheck({AuthType.ROLE_ADMIN, AuthType.ROLE_VERIFY_USER})
+    @Transactional
     @Override
     public void sendMessage(MessageRequest request, MemberVO member) {
+        permissionCheck.hasPermission(() -> chatService.getChatMember(request.chatId(), member.getId()) == null,
+                new ChatException(ChatExceptionType.NOT_EXISTED_CHAT_MEMBER));
+
         MessageVO message = MessageVO.builder()
                 .content(request.content())
                 .sender(member)
@@ -34,10 +41,11 @@ public class MessageServiceImpl implements MessageService {
         messageMapper.sendMessage(message);
     }
 
-    //    @AuthCheck({AuthType.ROLE_ADMIN, AuthType.ROLE_VERIFY_USER})
+    @AuthCheck({AuthType.ROLE_ADMIN, AuthType.ROLE_VERIFY_USER})
+    @Transactional
     @Override
     public List<MessageVO> getMessageByChat(Long chatId, MemberVO member) {
-        permissionCheck.hasPermission(() -> chatService.getChatMember(chatId, member.getId()) != null,
+        permissionCheck.hasPermission(() -> chatService.getChatMember(chatId, member.getId()) == null,
                 new ChatException(ChatExceptionType.NOT_EXISTED_CHAT_MEMBER));
 
         chatMemberMapper.updateLastReadAt(chatId, member.getId(), new Timestamp(System.currentTimeMillis()));
