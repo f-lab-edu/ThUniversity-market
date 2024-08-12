a5c0ea23514691d9e86bc000c156d8efa0cc3334
kbs1027 <rlaqjatn1027@gmail.com>
2024-08-12 21:17:42 +0900

    refactor : multi module 분리

--- market-api/src/main/java/university/market/MarketApplication.java
+++ market-api/src/main/java/university/market/MarketApplication.java
@@ -1,5 +1,7 @@
 package university.market;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -9,5 +11,10 @@ import org.springframework.context.annotation.PropertySource;
 public class MarketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MarketApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 }
