76694af3d84ed56b1a8ed1d77f49858e281cdd97
kbs1027 <rlaqjatn1027@gmail.com>
2024-08-22 17:29:21 +0900

    feat : 빌더 추가

--- market-common/src/main/java/university/market/tag/service/dto/request/TagMemberRequest.java
+++ market-common/src/main/java/university/market/tag/service/dto/request/TagMemberRequest.java
@@ -1,7 +1,9 @@
 package university.market.tag.service.dto.request;
 
+import lombok.Builder;
 import university.market.member.domain.MemberVO;
 
+@Builder
 public record TagMemberRequest(
         long tagId,
         MemberVO member
