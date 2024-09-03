9e471fca90c4ba27b7bae7dd00a39eb3de4de485
kbs1027 <rlaqjatn1027@gmail.com>
2024-09-03 15:04:49 +0900

    feat : batch service init

---
+++ market-batch/src/main/java/sparkling/marketbatch/MarketBatchApplication.java
@@ -0,0 +1,13 @@
+package sparkling.marketbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketBatchApplication.class, args);
+    }
+
+}
---
+++ market-batch/src/test/java/sparkling/marketbatch/MarketBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package sparkling.marketbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
